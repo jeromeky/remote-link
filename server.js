98d13f09c68632ba390a827c46fc7990649a3e71
Jerome Ky <jerome@rezdy.com>
2016-11-25 12:33:50 +1100

    add body parser

--- server.js
+++ server.js
@@ -6,8 +6,15 @@
 // call the packages we need
 var express    = require('express');
 var app        = express();
+var bodyParser = require('body-parser');
 var opener = require('opener');
 var childProcess = require("child_process");
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 var port = process.env.PORT || 8888;        // set our port
@@ -17,7 +24,7 @@ var port = process.env.PORT || 8888;        // set our port
 var router = express.Router();              // get an instance of the express Router
 var random = "123";
 
-router.route('/spotify')
+router.route('/music')
 
     .get(function(req, res) {
     	console.log(req.query.link);
